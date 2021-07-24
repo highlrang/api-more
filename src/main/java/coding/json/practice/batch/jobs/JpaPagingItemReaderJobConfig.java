5fd3ddf112453d806db7ede0780bcec4b98a8d49
jhw127 <jhw127@naver.com>
2021-07-24 23:54:23 +0900

    spring batch study

---
+++ src/main/java/coding/json/practice/batch/jobs/JpaPagingItemReaderJobConfig.java
@@ -0,0 +1,74 @@
+package coding.json.practice.batch.jobs;
+
+import coding.json.practice.batch.Pay;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+
+
+// Reader와 Processor(chunkProvider.provide())에서는 1건씩 다뤄지고(add), Writer(chunkProvider.process())에선 Chunk 단위로 처리
+// Chunk Size는 한번에 처리될 트랜잭션 단위를 얘기하며, Page Size는 한번에 조회할 Item의 양
+// Tasklet 중에서 ChunkOrientedTasklet을 통해 Chunk를 처리하며 이를 구성하는 3 요소로 ItemReader, ItemWriter, ItemProcessor
+// 웬만하면 JdbcItemReader (querydql의 경우 HibernateItemReader)
+
+// cursor 기반과 paging 기반
+// (전자는 ResultSet과 직접 연동하여 데이터를 읽어오는 것인데, 일종의 stream 과 같다고 보시면 됩니다.
+// 사실상 전체를 조회하여 stream처럼 지속적으로 데이터를 가져오는 방식이기에 페이징 이슈는 발생하지 않습니다.
+// 또한, 성능 역시 PagingItemReader보다 좋습니다.
+// 후자는 대량의 데이터, 멀티쓰레드 환경에서 사용. JPA에서는 paging만 지원)
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class JpaPagingItemReaderJobConfig {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    private int chunkSize = 10;
+
+    @Bean
+    public Job jpaPagingItemReaderJob(){
+        return jobBuilderFactory.get("jpaPagingItemReaderJob")
+                .start(jpaPagingItemReaderStep())
+                .build();
+    }
+
+    @Bean
+    public Step jpaPagingItemReaderStep(){
+        return stepBuilderFactory.get("jpaPagingItemReaderStep")
+                .<Pay, Pay>chunk(chunkSize)
+                .reader(jpaPagingItemReader())
+                .writer(jpaPagingItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JpaPagingItemReader<Pay> jpaPagingItemReader(){
+        return new JpaPagingItemReaderBuilder<Pay>()
+                .name("jpaPagingItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(chunkSize)
+                // pageSize chunkSize
+                .queryString("select p from Pay p where amount >= 2000") // order by 필요 !!
+                .build();
+    }
+
+    private ItemWriter<Pay> jpaPagingItemWriter(){ // list(chunk)
+        return list -> {
+            for (Pay pay:list){
+                log.info("Current Pay = {}", pay);
+            }
+        };
+    }
+
+}
---
+++ src/main/java/coding/json/practice/batch/jobs/SimpleJobConfig.java
@@ -0,0 +1,62 @@
+package coding.json.practice.batch.jobs;
+
+import lombok.RequiredArgsConstructor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// Batch > Job > Step > Tasklet // 동일한 파라미터 실행 안 함
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class SimpleJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job simpleJob(){
+        return jobBuilderFactory.get("simpleJob")
+                .start(simpleStep1(null))
+                .next(simpleStep2(null))
+                .build();
+    }
+
+    @Bean
+    @JobScope // Step에서 사용 가능
+    // @StepScope >> jobParameters, jobExecutionContext, stepExecutionContext
+    // Tasklet, Item... 등에서 사용 가능
+    // @Bean과 함께 사용할 경우 proxy 모드 (read만 가능) >> ItemReader 등이 아닌 실제 return 객체를 명시하기 (JpaPagingItemReader<T>)
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate){
+        return stepBuilderFactory.get("simpleStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>> Step1, requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                    // throw new IllegalArgumentException("step1에서 실패");
+                })
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate){
+        return stepBuilderFactory.get("simpleStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>> Step2, requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    // job name 지정 > yml에 spring.batch.job.names: ${job.name:NONE}
+    // 실행 > java -jar batch-application.jar --job.name=simpleJob
+}
